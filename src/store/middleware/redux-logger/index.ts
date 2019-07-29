4634fcfd824029980df1bb11b24a57a9e4567505
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-29 17:24:31 +0300

    Add store middlewares and reduxLogger

---
+++ src/store/middleware/redux-logger/index.ts
@@ -0,0 +1,16 @@
+import { createLogger } from 'redux-logger';
+
+const reduxLogger = createLogger({
+	duration: true,
+	collapsed: true,
+	diff: true,
+	colors: {
+		title: () => '#139BFE',
+		prevState: () => '#1C5FAF',
+		action: () => '#149945',
+		nextState: () => '#A47104',
+		error: () => '#ff0005',
+	},
+});
+
+export { reduxLogger };
