24a517cc403a219d306abfbedb93d6461fe7b373
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-30 17:00:10 +0300

    Cleanup

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
+/* npm imports: common */
 import { createStore, compose, applyMiddleware, Middleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
+/* root imports: common */
+import { reduxLogger } from 'config/middleware';
 import { rootReducer } from 'reducers';
-import { reduxLogger } from 'store/middleware';
 import { saga } from 'sagas';
 
 const dev = process.env.NODE_ENV === 'development';
