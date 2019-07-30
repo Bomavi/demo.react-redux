a7f9fe6a76f59428a57330e9bcfd0c4e783d69e6
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-30 16:59:29 +0300

    Add root saga and include others

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,9 +1,20 @@
-// Core
+/* npm imports: common */
 import { all } from 'redux-saga/effects';
 
-// Instruments
+/* local imports: common */
+import auth from './auth';
 import tasks from './tasks';
 
 export function* saga() {
-	yield all([tasks.fetchWatcher()]);
+	yield all([
+		auth.authenticateWatcher(),
+		auth.loginWatcher(),
+		auth.registerWatcher(),
+		auth.logoutWatcher(),
+
+		tasks.fetchWatcher(),
+		tasks.createWatcher(),
+		tasks.updateWatcher(),
+		tasks.deleteWatcher(),
+	]);
 }
