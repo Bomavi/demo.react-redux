090a461898c9999f926bd3b98b345b8aaa0dc0a8
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-30 16:53:45 +0300

    Change prettierrc extension

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    trailingComma: 'es5',
-    useTabs: true,
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
-    printWidth: 100,
-    ignorePath: '.prettierignore',
-};
