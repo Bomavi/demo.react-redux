d8933840c1b1936b19510b4b0b19f64becdfb9ee
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-30 16:58:12 +0300

    Combine reducers to rootReducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,11 +1,13 @@
+/* npm imports: common */
 import { combineReducers } from 'redux';
 
+/* local imports: common */
 import { auth } from './auth';
 import { tasks } from './tasks';
 
-const rootReducer = combineReducers({
+export const rootReducer = combineReducers({
 	auth,
 	tasks,
 });
 
-export { rootReducer };
+export type State = ReturnType<typeof rootReducer>;
