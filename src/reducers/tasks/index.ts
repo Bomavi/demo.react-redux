abbdbc5a9ce3c2beccc5bbe2e4d4ac81384a7f48
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-30 16:57:45 +0300

    Add tasks reducer

--- src/reducers/tasks/index.ts
+++ src/reducers/tasks/index.ts
@@ -1,20 +1,105 @@
+/* npm imports: common */
 import { Reducer } from 'redux';
 
+/* root imports: common */
+import * as types from 'actions/tasks/types';
+
 export interface TasksState {
-	taskList: TaskType[];
+	taskList: readonly TaskType[];
+	search: TasksSearchType;
+	isFetching: boolean;
+	inProgress: boolean;
 }
 
 const initialState: TasksState = {
 	taskList: [],
+	search: {
+		q: '',
+	},
+	isFetching: false,
+	inProgress: false,
 };
 
-const tasks: Reducer<TasksState> = (state = initialState, action) => {
+const tasks: Reducer<TasksState, types.TaskActionTypes> = (
+	state = initialState,
+	action
+): TasksState => {
 	switch (action.type) {
-		case 'SET_TASKS':
+		case types.SEARCH_TASKS: {
 			return {
 				...state,
-				tasks: action.payload,
+				search: {
+					q: action.payload,
+				},
 			};
+		}
+
+		case types.SET_ISFETCHING: {
+			return {
+				...state,
+				isFetching: action.payload,
+			};
+		}
+
+		case types.SET_INPROGRESS: {
+			return {
+				...state,
+				inProgress: action.payload,
+			};
+		}
+
+		case types.SET_UPDATE_INPROGRESS: {
+			const { id, inProgress } = action.payload;
+			return {
+				...state,
+				taskList: state.taskList.map(t =>
+					t._id === id ? { ...t, updateInProgress: inProgress } : t
+				),
+			};
+		}
+
+		case types.SET_DELETE_INPROGRESS: {
+			const { id, inProgress } = action.payload;
+			return {
+				...state,
+				taskList: state.taskList.map(t =>
+					t._id === id ? { ...t, deleteInProgress: inProgress } : t
+				),
+			};
+		}
+
+		case types.FETCH_TASKS_ONSUCCESS: {
+			return {
+				...state,
+				taskList: action.payload,
+			};
+		}
+
+		case types.CREATE_TASK_ONSUCCESS: {
+			return {
+				...state,
+				taskList: [...state.taskList, action.payload],
+			};
+		}
+
+		case types.UPDATE_TASK_ONSUCCESS: {
+			const task = action.payload;
+			return {
+				...state,
+				taskList: state.taskList.map(t =>
+					t._id === task._id ? task : t
+				),
+			};
+		}
+
+		case types.DELETE_TASK_ONSUCCESS: {
+			const id = action.payload;
+			return {
+				...state,
+				taskList: state.taskList.filter(t => t._id !== id),
+			};
+		}
+
 		default:
 			return state;
 	}
