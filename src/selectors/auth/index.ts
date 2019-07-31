8eb2a37a8900ee055ba881761f450e897a49cfae
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-31 17:25:47 +0300

    Add selectors and auth selector

---
+++ src/selectors/auth/index.ts
@@ -0,0 +1,49 @@
+/* npm imports: common */
+import { createSelector, Selector } from 'reselect';
+
+/* npm imports: material-ui/core */
+import { Theme } from '@material-ui/core/styles';
+
+/* root imports: common */
+import { State } from 'reducers';
+import { lightTheme, darkTheme } from 'utils/themes';
+
+const userSelector = (state: State) => state.auth.user;
+const themeSelector = (state: State) => state.auth.theme.selectedThemeType;
+
+export const getIsAuthenticated: Selector<State, boolean> = createSelector(
+	userSelector,
+	user => !!user
+);
+
+export const getSelectedTheme: Selector<State, Theme> = createSelector(
+	themeSelector,
+	theme => {
+		switch (theme) {
+			case 'light':
+				return lightTheme;
+
+			case 'dark':
+				return darkTheme;
+
+			default:
+				return lightTheme;
+		}
+	}
+);
+
+export const getThemeNameToSwitch: Selector<State, MUIThemeType> = createSelector(
+	themeSelector,
+	theme => {
+		switch (theme) {
+			case 'light':
+				return 'dark';
+
+			case 'dark':
+				return 'light';
+
+			default:
+				return 'light';
+		}
+	}
+);
