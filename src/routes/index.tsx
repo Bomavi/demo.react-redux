9ff03051e560ee3c299786a21f91da279929cd6f
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-01 22:07:12 +0300

    Add new routes as protected and public

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,62 @@
-export * from './Authorization';
-export * from './Protected';
-export * from './Public';
+/* npm imports: common */
+import React from 'react';
+import { connect } from 'react-redux';
+import { Router, Switch, Route, Redirect } from 'react-router';
+
+/* root imports: view components */
+import { Login } from 'features/Login';
+import { Home } from 'features/Home';
+
+/* root imports: common */
+import { history } from 'config/history';
+import { State } from 'reducers';
+import {
+	getAccessibleOnlyForAuthorized,
+	getAccessibleOnlyForUnauthorized,
+	getNotAccessibleForAuthorized,
+	getNotAccessibleForUnauthorized,
+} from 'selectors';
+
+const mapStateToProps = (state: State) => ({
+	accessibleOnlyForAuthorized: getAccessibleOnlyForAuthorized(state),
+	accessibleOnlyForUnauthorized: getAccessibleOnlyForUnauthorized(state),
+	notAccessibleForAuthorized: getNotAccessibleForAuthorized(state),
+	notAccessibleForUnauthorized: getNotAccessibleForUnauthorized(state),
+});
+
+const mapDispatchToProps = () => ({});
+
+type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
+
+class RoutesComponent extends React.Component<Props> {
+	public render() {
+		const {
+			accessibleOnlyForAuthorized,
+			accessibleOnlyForUnauthorized,
+			notAccessibleForAuthorized,
+			notAccessibleForUnauthorized,
+		} = this.props;
+
+		return (
+			<Router history={history}>
+				<Switch>
+					{accessibleOnlyForAuthorized && (
+						<Route exact path="/" component={Home} />
+					)}
+					{accessibleOnlyForUnauthorized && (
+						<Route exact path="/login" component={Login} />
+					)}
+					{notAccessibleForAuthorized && <Redirect to="/" />}
+					{notAccessibleForUnauthorized && <Redirect to="/login" />}
+				</Switch>
+			</Router>
+		);
+	}
+}
+
+const RoutesWithRedux = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(RoutesComponent);
+
+export const Routes = RoutesWithRedux;
