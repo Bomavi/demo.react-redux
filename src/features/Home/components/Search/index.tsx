60675bd2e7cbb28e80c3a899ee5e49a61b41e4fc
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-01 22:12:57 +0300

    Add Search component

---
+++ src/features/Home/components/Search/index.tsx
@@ -0,0 +1,71 @@
+/* npm imports: common */
+import React from 'react';
+
+/* npm imports: material-ui/core */
+import { withStyles, WithStyles } from '@material-ui/core/styles';
+
+/* root imports: view components */
+import { CustomInput } from 'views/elements';
+
+/* root imports: common */
+import { searchTasks, fetchTasks } from 'actions/tasks';
+import { debounce, debounceTiming } from 'utils/helpers';
+
+/* local imports: common */
+import { styles } from './styles';
+
+interface SearchProps extends WithStyles<typeof styles> {
+	isFetching: boolean;
+	searchTasks: typeof searchTasks;
+	fetchTasks: typeof fetchTasks;
+}
+
+interface SearchState {
+	searchQuery: string;
+}
+
+class SearchComponent extends React.Component<SearchProps> {
+	public state = {
+		searchQuery: '',
+	};
+
+	public componentWillUnmount() {
+		if (this.state.searchQuery.length > 0) {
+			this.props.searchTasks('');
+			this.props.fetchTasks();
+		}
+	}
+
+	public changeHandler = debounce((value: string) => {
+		this.setState(
+			() => ({
+				searchQuery: value,
+			}),
+			() => {
+				this.props.searchTasks(value);
+				this.props.fetchTasks();
+			}
+		);
+	}, debounceTiming.input);
+
+	public render() {
+		// const { classes } = this.props;
+		const { isFetching } = this.props;
+
+		return (
+			<CustomInput
+				icon={{
+					name: 'magnify',
+					svgSize: 'md',
+				}}
+				isFetching={isFetching}
+				placeholder="Type to search tasks..."
+				onChange={this.changeHandler}
+			/>
+		);
+	}
+}
+
+const Search = withStyles(styles)(SearchComponent);
+
+export { Search };
