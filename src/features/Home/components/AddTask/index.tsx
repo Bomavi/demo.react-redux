b4896c2bb1a43e0bf168a1831189cc2e279afd1f
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-01 22:12:40 +0300

    Add AddTask component

---
+++ src/features/Home/components/AddTask/index.tsx
@@ -0,0 +1,46 @@
+/* npm imports: common */
+import React from 'react';
+
+/* npm imports: material-ui/core */
+import { withStyles, WithStyles } from '@material-ui/core/styles';
+
+/* root imports: view components */
+import { CustomInput } from 'views/elements';
+
+/* root imports: common */
+import { createTask } from 'actions/tasks';
+
+/* local imports: common */
+import { styles } from './styles';
+
+interface AddTaskProps extends WithStyles<typeof styles> {
+	inProgress: boolean;
+	createTask: typeof createTask;
+}
+
+class AddTaskComponent extends React.Component<AddTaskProps> {
+	public actionHandler = (value: string) => {
+		this.props.createTask({ description: value, completed: false });
+	};
+
+	public render() {
+		// const { classes } = this.props;
+		const { inProgress } = this.props;
+
+		return (
+			<CustomInput
+				icon={{
+					name: 'plus',
+					svgSize: 'md',
+				}}
+				isFetching={inProgress}
+				placeholder="Type task description..."
+				onClick={this.actionHandler}
+			/>
+		);
+	}
+}
+
+const AddTask = withStyles(styles)(AddTaskComponent);
+
+export { AddTask };
