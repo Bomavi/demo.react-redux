158732dd587df57ab10851c18ae83125c550d0c7
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-02 00:43:17 +0300

    Modify placeholder

--- src/features/Home/components/Search/index.tsx
+++ src/features/Home/components/Search/index.tsx
@@ -59,7 +59,7 @@ class SearchComponent extends React.Component<SearchProps> {
 					svgSize: 'md',
 				}}
 				isFetching={isFetching}
-				placeholder="Type to search tasks..."
+				placeholder="Type to search..."
 				onChange={this.changeHandler}
 			/>
 		);
