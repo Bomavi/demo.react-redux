1c5c28854bfc9b938747d3157f6c39320fb83a82
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-02 00:41:16 +0300

    Add sort selector by sortKey

--- src/selectors/tasks/index.ts
+++ src/selectors/tasks/index.ts
@@ -5,13 +5,16 @@ import { createSelector, Selector } from 'reselect';
 import { State } from 'reducers';
 
 const tasksSelector = (state: State) => state.tasks.taskList;
+const sortKeySelector = (state: State) => state.tasks.sort;
 
 export const getSortedByDateTasks: Selector<State, TaskType[]> = createSelector(
-	[tasksSelector],
-	tasks =>
-		tasks.sort(
-			(a, b) => Number(new Date(b.createdAt)) - Number(new Date(a.createdAt))
-		)
+	[tasksSelector, sortKeySelector],
+	(tasks, sortKey) =>
+		tasks.sort((a, b) => {
+			const aDate = Number(new Date(a.createdAt));
+			const bDate = Number(new Date(b.createdAt));
+			return sortKey === 'desc' ? bDate - aDate : aDate - bDate;
+		})
 );
 
 export const getSortedTasks: Selector<State, TaskType[]> = createSelector(
