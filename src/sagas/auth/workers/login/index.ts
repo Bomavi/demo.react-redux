9671300cc8a7c3f1a123ee44da423388ca09a4db
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-01 22:16:39 +0300

    Cleanup and add redirect

--- src/sagas/auth/workers/login/index.ts
+++ src/sagas/auth/workers/login/index.ts
@@ -3,12 +3,8 @@ import { call, put } from 'redux-saga/effects';
 
 /* root imports: common */
 import { services } from 'config/services';
-import {
-	setInProgress,
-	LoginAction,
-	loginOnSuccess,
-	loginOnFail,
-} from 'actions/auth';
+import { history } from 'config/history';
+import { setInProgress, LoginAction, loginOnSuccess, loginOnFail } from 'actions/auth';
 
 export function* loginWorker({ payload }: LoginAction) {
 	yield put(setInProgress(true));
@@ -17,6 +13,7 @@ export function* loginWorker({ payload }: LoginAction) {
 		const user = yield call(services.auth.login, payload);
 
 		yield put(loginOnSuccess(user));
+		yield call(history.push, '/' as any);
 	} catch (e) {
 		yield put(loginOnFail(e.message));
 	} finally {
