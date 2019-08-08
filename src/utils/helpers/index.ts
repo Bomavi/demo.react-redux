91755d2e6b910cda19c82b7496cd6c484aaa1964
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-08 23:37:26 +0300

    Add new exports to helpers exporter

--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -3,3 +3,5 @@ export * from './query-string';
 export * from './maybe';
 export * from './validator';
 export * from './remove-spaces';
+export * from './record-saga';
+export * from './handle-error-logger';
