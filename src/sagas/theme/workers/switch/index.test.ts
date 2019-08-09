b0b750be487bf95e3f8559936b8dccd80c65f870
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-09 23:56:36 +0300

    Improve switchTheme saga test, add afterEach/All

--- src/sagas/theme/workers/switch/index.test.ts
+++ src/sagas/theme/workers/switch/index.test.ts
@@ -6,32 +6,43 @@ import { mockedUsersServerResponse } from '__mocks__/services/users';
 import { recordSaga } from 'utils/helpers';
 import { setThemeInProgress } from 'actions/theme';
 import { updateUserOnSuccess, updateUserOnFail } from 'actions/user';
+import * as types from 'actions/user/types';
 
 /* local imports: common */
 import { switchWorker } from '.';
 
-describe('Saga: SWITCH_THEME and UPDATE_USER:', () => {
-	const payload = 'light';
+describe('Saga: SWITCH_THEME', () => {
+	afterEach(() => {
+		mockedUsersServerResponse.reset();
+	});
 
-	it('UPDATE_USER_ON_FAIL:', async () => {
+	afterAll(() => {
+		mockedUsersServerResponse.restore();
+	});
+
+	const payload = user.theme;
+
+	test(types.UPDATE_USER_ON_FAIL, async () => {
 		mockedUsersServerResponse.initFailResponse();
 		const dispatched = await recordSaga(switchWorker, { payload });
-		mockedUsersServerResponse.reset();
+		const toEqual = [
+			setThemeInProgress(true),
+			updateUserOnFail('Error: Network Error'),
+			setThemeInProgress(false),
+		];
 
-		expect(dispatched).toContainEqual(setThemeInProgress(true));
-		expect(dispatched).toContainEqual(updateUserOnFail('Error: Network Error'));
-		expect(dispatched).toContainEqual(setThemeInProgress(false));
+		expect(dispatched).toEqual(toEqual);
 	});
 
-	it('UPDATE_USER_ON_SUCCESS:', async () => {
+	test(types.UPDATE_USER_ON_SUCCESS, async () => {
 		mockedUsersServerResponse.initSuccessResponse();
 		const dispatched = await recordSaga(switchWorker, { payload });
-		mockedUsersServerResponse.reset();
+		const toEqual = [
+			setThemeInProgress(true),
+			updateUserOnSuccess(user),
+			setThemeInProgress(false),
+		];
 
-		expect(dispatched).toContainEqual(setThemeInProgress(true));
-		expect(dispatched).toContainEqual(updateUserOnSuccess(user));
-		expect(dispatched).toContainEqual(setThemeInProgress(false));
+		expect(dispatched).toEqual(toEqual);
 	});
-
-	mockedUsersServerResponse.restore();
 });
