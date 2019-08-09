2f2445905798320f204aace163d558d35c964a66
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-09 23:55:48 +0300

    Improve login saga test, add afterEach/All

--- src/sagas/auth/workers/login/index.test.ts
+++ src/sagas/auth/workers/login/index.test.ts
@@ -5,30 +5,37 @@ import { mockedAuthServerResponse } from '__mocks__/services/auth';
 /* root imports: common */
 import { recordSaga } from 'utils/helpers';
 import { setInProgress, loginOnSuccess, loginOnFail } from 'actions/auth';
+import * as types from 'actions/auth/types';
 
 /* local imports: common */
 import { loginWorker } from '.';
 
-describe('Saga: LOGIN:', () => {
-	it('LOGIN_ON_FAIL:', async () => {
+describe('Saga: LOGIN', () => {
+	afterEach(() => {
+		mockedAuthServerResponse.reset();
+	});
+
+	afterAll(() => {
+		mockedAuthServerResponse.restore();
+	});
+
+	test(types.LOGIN_ON_FAIL, async () => {
 		mockedAuthServerResponse.initFailResponse();
 		const dispatched = await recordSaga(loginWorker);
-		mockedAuthServerResponse.reset();
+		const toEqual = [
+			setInProgress(true),
+			loginOnFail('Error: Network Error'),
+			setInProgress(false),
+		];
 
-		expect(dispatched).toContainEqual(setInProgress(true));
-		expect(dispatched).toContainEqual(loginOnFail('Error: Network Error'));
-		expect(dispatched).toContainEqual(setInProgress(false));
+		expect(dispatched).toEqual(toEqual);
 	});
 
-	it('LOGIN_ON_SUCCESS:', async () => {
+	test(types.LOGIN_ON_SUCCESS, async () => {
 		mockedAuthServerResponse.initSuccessResponse();
 		const dispatched = await recordSaga(loginWorker);
-		mockedAuthServerResponse.reset();
+		const toEqual = [setInProgress(true), loginOnSuccess(user), setInProgress(false)];
 
-		expect(dispatched).toContainEqual(setInProgress(true));
-		expect(dispatched).toContainEqual(loginOnSuccess(user));
-		expect(dispatched).toContainEqual(setInProgress(false));
+		expect(dispatched).toEqual(toEqual);
 	});
-
-	mockedAuthServerResponse.restore();
 });
